67c45827437cea2e7567eda199f4b2bf811b4796
south-maple <1739882982@qq.com>
2021-02-03 09:07:26 +0800

    format

--- cloud-api-common/src/main/java/com/atguigu/springcloud/entity/Payment.java
+++ cloud-api-common/src/main/java/com/atguigu/springcloud/entity/Payment.java
@@ -8,17 +8,11 @@ import java.io.Serializable;
  * (Payment)表实体类
  *
  * @author SouthMaple
- * @since 2021-01-25 11:25:26
+ * @date  2021-01-25
  */
 @Data
 public class Payment implements Serializable {
     private static final long serialVersionUID = 1L;
-
-    /**
-     * ID
-     */
     private Long id;
-
     private String serial;
-
 }
--- cloud-consumerconsul-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
+++ cloud-consumerconsul-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -1,4 +1,4 @@
-package springcloud.controller;
+package com.atguigu.springcloud.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;

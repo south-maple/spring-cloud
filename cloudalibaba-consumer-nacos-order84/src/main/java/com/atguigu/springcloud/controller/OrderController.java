98767458ac6ab537a3a3a4c2a448af76a1444f8e
south-maple <1739882982@qq.com>
2021-02-03 14:46:02 +0800

    format

--- cloudalibaba-consumer-nacos-order84/src/main/java/com/atguigu/springcloud/controller/OrderController.java
+++ cloudalibaba-consumer-nacos-order84/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -1,6 +1,7 @@
 package com.atguigu.springcloud.controller;
 
 import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.atguigu.springcloud.entity.CommonResult;
 import com.atguigu.springcloud.entity.Payment;
 import com.atguigu.springcloud.service.PaymentService;
@@ -26,8 +27,40 @@ public class OrderController {
     @Resource
     private PaymentService paymentService;
 
+//--------------------------------- Ribbon ---------------------------------
+
+    @RequestMapping("/consumer/fallback/{id}")
+    @SentinelResource(value = "fallback",
+            fallback = "handlerFallback",
+            blockHandler = "blockHandler",
+            exceptionsToIgnore = {IllegalArgumentException.class}
+    )//fallback和blockHandler 都配置
+    public CommonResult<Payment> paymentByRibbon(@PathVariable("id") Long id) {
+        CommonResult<Payment> result = restTemplate.getForObject(serviceUrl + "/paymentSQL/" + id, CommonResult.class, id);
+        if (id == 4) {
+            throw new IllegalArgumentException("IllegalArgumentException,非法参数异常");
+        } else if (result.getData() == null) {
+            throw new NullPointerException("NullPointerException,该ID没有对应记录，空指针异常");
+        }
+        return result;
+    }
+
+    // 本例子是 fallback handlerFallback
+    public CommonResult handlerFallback(@PathVariable("id") Long id, Throwable e) {
+        Payment payment = new Payment(id, "null");
+        return new CommonResult(444, "兜底异常handlerFallback，exception内容" + e.getMessage(), payment);
+    }
+
+    // 本例子是 blockHandler
+    public CommonResult blockHandler(@PathVariable("id") Long id, BlockException blockException) {
+        Payment payment = new Payment(id, "null");
+        return new CommonResult(445, "blockHandler-sentinel限流，无此流水：blockException" + blockException.getClass().getCanonicalName(), payment);
+    }
+
+//--------------------------------- OpenFeign ---------------------------------
+
     @GetMapping(value = "/consumer/paymentSQL/{id}")
-    public CommonResult<Payment> paymentSQL(@PathVariable("id") Long id) {
+    public CommonResult<Payment> paymentSQLByFeign(@PathVariable("id") Long id) {
         return paymentService.paymentSQL(id);
     }
 }
